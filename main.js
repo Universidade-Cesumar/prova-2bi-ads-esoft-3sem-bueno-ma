33f99de842fe5fa5ac08a1d9afe981167c74e872
bueno-ma <maria.eduardaazevedo270@gmail.com>
2026-06-17 19:45:38 -0300

    Incluão função delete

--- main.js
+++ main.js
@@ -1,40 +1,86 @@
-const URL_API = "https://6a29fdc6f59cb8f65f1debed.mockapi.io/materiais";
+tabela.appendChild(linha);
+    });
 
-function validarRetirada(estoqueAtual, quantidadeRetirada) {
-
-    if (quantidadeRetirada <= 0) {
-        return false;
-    }
-
-    if (quantidadeRetirada > estoqueAtual) {
-        return false;
-    }
-
-    return true;
+    configurarEventos();
 }
 
-function atualizarTabela(materiais) {
+async function carregarMateriais() {
 
-    const tabela = document.getElementById("lista-materiais");
+    try {
 
-    if (!tabela) {
-        console.error("Tabela não encontrada!");
-        return;
+        const resposta = await fetch(URL_API);
+
+        if (!resposta.ok) {
+            throw new Error(`Erro HTTP: ${resposta.status}`);
+        }
+
+        const materiais = await resposta.json();
+
+        atualizarTabela(materiais);
+
+    } catch (erro) {
+
+        console.error("Erro ao carregar materiais:", erro);
+
+        const tabela = document.getElementById("lista-materiais");
+
+        if (tabela) {
+
+            tabela.innerHTML = `
+                <tr>
+                    <td colspan="3" class="text-danger text-center">
+                        Erro ao carregar dados
+                    </td>
+                </tr>
+            `;
+        }
     }
+}
 
-    tabela.innerHTML = "";
+async function adicionarMaterial(nome, quantidade) {
 
-    if (!Array.isArray(materiais) || materiais.length === 0) {
+    try {
 
-        tabela.innerHTML = `
-            <tr>
-                <td colspan="3" class="text-center">
-                    Nenhum material cadastrado
-                </td>
-            </tr>
-        `;
+        const resposta = await fetch(URL_API, {
 
-        return;
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                nome: nome,
+                quantidade: Number(quantidade)
+            })
+        });
+
+        if (!resposta.ok) {
+            throw new Error(`Erro HTTP: ${resposta.status}`);
+        }
+
+        await carregarMateriais();
+
+    } catch (erro) {
+
+        console.error("Erro ao adicionar material:", erro);
+
+        alert("Erro ao adicionar material.");
     }
+}
 
-    materiais.forEach(material =>
+async function excluirMaterial(id) {
+
+    try {
+
+        await fetch(`${URL_API}/${id}`, {
+            method: "DELETE"
+        });
+
+        await carregarMateriais();
+
+    } catch (erro) {
+
+        console.error("Erro ao excluir:", erro);
+    }
+}
